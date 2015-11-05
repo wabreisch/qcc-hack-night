342657fe87d95a2a16cfca9e41d044d5a76f2290
wabreisch <wabreisch@quinnipiac.edu>
2015-11-04 20:52:35 -0500

    Initial commit

--- qcc.js
+++ qcc.js
@@ -1,11 +1,21 @@
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
+    teams: [
       { text: "Nashville Predators"},
       { text: "Los Angeles Kings"},
+      { text: "New York Mets"},
+      { text: "Boston Bruins"},
       { text: "Philadelphia Flyers"}
     ]
-  }); 
+  });
+
+  Template.body.events({
+    "submit form": function (e) {
+      e.preventDefault();
+      teamname = $(e.target).find('[id=textinput]').val();
+      alert(teamname);
+    }
+  });
 }
 
 if (Meteor.isServer) {
